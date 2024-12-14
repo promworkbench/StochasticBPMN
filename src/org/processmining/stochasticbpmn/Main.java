7b624c76e16266059a2909439bce32185f64c768
Aleksandar Kuzmanoski <aleksandarkuzmanoski11@gmail.com>
2024-12-14 20:11:17 +0100

    Fix SPN 2 SBPMN experiment runner

--- src/org/processmining/stochasticbpmn/Main.java
+++ src/org/processmining/stochasticbpmn/Main.java
@@ -1,23 +1,19 @@
 package org.processmining.stochasticbpmn;
 
-import org.processmining.stochasticbpmn.algorithms.converter.StochasticAPNToStochasticBPMNConverter;
 import org.processmining.stochasticbpmn.algorithms.diagram.reader.StochasticBPMNDiagramFromSPNReader;
 import org.processmining.stochasticbpmn.algorithms.reader.ObjectReader;
-import org.processmining.stochasticbpmn.algorithms.reader.StochasticAPNReader;
 import org.processmining.stochasticbpmn.models.graphbased.directed.bpmn.stochastic.StochasticBPMNDiagram;
 import org.processmining.stochasticbpmn.models.graphbased.directed.bpmn.stochastic.StochasticGateway;
-import org.processmining.stochasticbpmn.models.petrinets.stochastic.StochasticAPN;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.nio.file.Files;
 import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         ObjectReader<File, StochasticBPMNDiagram> reader = StochasticBPMNDiagramFromSPNReader.fromFile();
-        for (String logName : new String[]{"road_fines", "sepsis", "bpic-2018-reference"} ) {
-            String path = "/home/aleks/Documents/DataResources/ProcessMining/StochasticPetriNets/Tobias/pn-slpn-evaluation-wawe/" + logName + "/hot-start";
+        for (String logName : new String[]{"road_fines", "sepsis", "bpic-2018-reference"}) {
+            String path = "/home/aleks/Documents/DataResources/ProcessMining/StochasticPetriNets/Tobias/pn-slpn" +
+                    "-evaluation-wawe/" + logName + "/hot-start";
             File dir = new File(path);
             File[] directoryListing = dir.listFiles();
             if (directoryListing != null) {
